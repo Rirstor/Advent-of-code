ddea0713f66be32431879f5441c527b6252a98a4
rirst <alexandre_boyer63@yahoo.fr>
2023-12-04 20:52:45 +0100

    Fixing day 3

--- 2023/day3/main.py
+++ 2023/day3/main.py
@@ -3,10 +3,12 @@ import numpy as np
 
 with open("day3/inputs/data_part1.txt", 'r') as file:
     lines = file.readlines()
-    length_line = len(lines[0]) - 1
+    length_line = len(lines[0])
 
-    whole_file = "".join(lines).replace("\n", "")
-    indices_numbers = [(elt.start(0), elt.end(0)) for elt in re.finditer(r"\d+", whole_file)]
+    result = dict()
+
+    whole_file = "".join(lines).replace("\n", ".")
+    indices_numbers = [(elt.start(), elt.end()) for elt in re.finditer(r"\d+", whole_file)]
     serial_numbers = list()
     for index in indices_numbers:
         index_range = np.arange(index[0] - 1, index[1] + 1)
@@ -14,8 +16,16 @@ with open("day3/inputs/data_part1.txt", 'r') as file:
             (index_range - length_line).tolist() + index_range.tolist() + (index_range + length_line).tolist())
         indices = indices[(indices >= 0) & (indices <= len(whole_file) - 1)]
         values = np.array([whole_file[idx] for idx in indices])
+        mask = [elt not in "0123456789." for elt in values]
 
-        if any([elt not in ".0123456789" for elt in values]):
-            serial_numbers.append(int("".join([whole_file[idx] for idx in np.arange(*index)])))
+        if any(mask):
+            number = int("".join([whole_file[idx] for idx in np.arange(*index)]))
+            idx = indices[[idx for idx, a in enumerate(mask) if a][0]]
 
-    print("The sum of the mechanical engine schematic is ", sum(serial_numbers))
+            if idx in result:
+                result[idx].append(number)
+            else:
+                result[idx] = [number]
+
+    print("The sum of the mechanical engine schematic is ", sum([sum(elt) for elt in result.values()]))
+    print("Part 2 answer : ", sum([np.prod(elt) for elt in result.values() if len(elt) == 2]))
