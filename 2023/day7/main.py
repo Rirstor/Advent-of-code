036e40f166b93b572310ed918e67091f78d160a2
rirst <alexandre_boyer63@yahoo.fr>
2023-12-07 23:04:19 +0100

    Day 7

--- 2023/day7/main.py
+++ 2023/day7/main.py
@@ -15,10 +15,11 @@ def process_card(card):
             output2[key] = 1
     if "J" in output2 and len(output2.keys()) != 1:
         value = output2.pop("J")
+
         if max(output2.values()) == 1:
             for k in ref:
                 if k in output2:
-                    output2[k] += 1
+                    output2[k] += value
                     break
         else:
             key = [key for key in output2 if output2[key] == max(output2.values())]
@@ -54,19 +55,3 @@ for nb_cards, different_cards in zip([5, 4, 3, 3, 2, 2, 1], [1, 2, 2, 3, 3, 4, 5
 
     rank -= len(sorted_cards)
 print(output)
-
-data = [l.strip().split(' ') for l in open('day7/input.txt')]
-
-def joker(a):
-    return max(a, key=a.get)
-
-def replace(a, o='AKQT98765432'):
-    return a.replace('J', joker({c: a.count(c) for c in o}))
-
-def score(a, b, o='AKQJT98765432'):
-    return [max(b.count(c) for c in b), -len(set(list(b)))] + [-o.index(c) for c in a]
-
-def win(a):
-    return sum(int(b) * (i + 1) for i, (_, b) in enumerate(a))
-
-print(win(sorted(data, key=lambda x: score(x[0], replace(x[0]), 'AKQT98765432J'))))
