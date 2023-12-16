8b1bd8b403268ed5438891d5132d0b009f0c902f
rirst <alexandre_boyer63@yahoo.fr>
2023-12-16 18:19:34 +0100

    Aled

--- 2023/day16/d16.py
+++ 2023/day16/d16.py
@@ -1,62 +1,74 @@
 import numpy as np
 import sys
 
-sys.setrecursionlimit(2166)
-puzzle = np.array([[i for i in l.split()[0]] for l in open("day16/input")])
+sys.setrecursionlimit(4000)
+puzzle = [[i for i in l.split()[0]] for l in open("day16/input")]
 
 directions = dict(N=(-1, 0), S=(1, 0), W=(0, -1), E=(0, 1))
 
+count = 0
 
-def propagate(point, d, puzzle, output=None):
+
+def propagate(point, d, output=None):
+    global count
+    count += 1
+    print(count)
     if output is None:
-        output = list()
-    next_point = point + np.array(directions[d])
+        output = dict()
+    offset_dir = directions[d]
+    next_point = (point[0] + offset_dir[0], point[1] + offset_dir[1])
 
     if any([(i < 0) or (i > len(puzzle) - 1) for i in next_point]):
         return None
-    npo = tuple(next_point.tolist())
-    if [npo, d] not in output:
-        output.append([npo, d])
+    # npo = tuple(next_point.tolist())
+
+    if next_point not in output:
+        output[next_point] = [d]
     else:
-        return None
-    sn = puzzle[next_point[0], next_point[1]]
+        d_seen = output[next_point]
+        if d not in d_seen:
+            d_seen.append(d)
+        else:
+            return None
+
+    sn = puzzle[next_point[0]][next_point[1]]
     if sn == ".":
-        propagate(next_point, d, puzzle, output)
+        propagate(next_point, d, output)
 
     elif sn == "/":
         if d == "E":
-            propagate(next_point, "N", puzzle, output)
+            propagate(next_point, "N", output)
         elif d == "W":
-            propagate(next_point, "S", puzzle, output)
+            propagate(next_point, "S", output)
         elif d == "N":
-            propagate(next_point, "E", puzzle, output)
+            propagate(next_point, "E", output)
         elif d == "S":
-            propagate(next_point, "W", puzzle, output)
+            propagate(next_point, "W", output)
     elif sn == "\\":
         if d == "E":
-            propagate(next_point, "S", puzzle, output)
+            propagate(next_point, "S", output)
         elif d == "W":
-            propagate(next_point, "N", puzzle, output)
+            propagate(next_point, "N", output)
         elif d == "N":
-            propagate(next_point, "W", puzzle, output)
+            propagate(next_point, "W", output)
         elif d == "S":
-            propagate(next_point, "E", puzzle, output)
+            propagate(next_point, "E", output)
 
     elif sn == "-":
         if d in ["N", "S"]:
             for nex_d in ["W", "E"]:
-                propagate(next_point, nex_d, puzzle, output)
+                propagate(next_point, nex_d, output)
         else:
-            propagate(next_point, d, puzzle, output)
+            propagate(next_point, d, output)
 
     elif sn == "|":
         if d in ["W", "E"]:
             for nex_d in ["N", "S"]:
-                propagate(next_point, nex_d, puzzle, output)
+                propagate(next_point, nex_d, output)
         else:
-            propagate(next_point, d, puzzle, output)
+            propagate(next_point, d, output)
 
 
-output = list()
-res = propagate(np.array([0, -1]), "E", puzzle, output)
-print(len(set(i[0] for i in output)))
+output = dict()
+res = propagate((0, -1), "E", output)
+print(len(set(output.keys())))
