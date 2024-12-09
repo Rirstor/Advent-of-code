dc8c1c0e6a7a4ff246088972f4fd8198478d17d6
rirst <alexandre_boyer63@yahoo.fr>
2024-12-09 23:42:39 +0100

    Day9 advent of code 2024 part2

--- 2024/day09/day09.py
+++ 2024/day09/day09.py
@@ -1,5 +1,6 @@
-data = [line.strip() for line in open("input")][0]
+from copy import copy
 
+data = [line.strip() for line in open("input")][0]
 
 # part 1
 
@@ -34,3 +35,46 @@ for id_number in sorted(list(positions.keys()), key=lambda x: int(x))[::-1]:
     positions[id_number] = pos_number
 part1 = sum(int(elt) * val for elt in positions.keys() for val in positions[elt])
 print(part1)
+
+# part2
+positions_points = list()
+positions = dict()
+offset = 0
+
+for id_elt in range(0, len(data) - 1, 2):
+    id_number = str(id_elt // 2)
+    nb_blocks = data[id_elt]
+    nb_free_space = int(data[id_elt + 1])
+    positions[id_number] = [offset + i for i in range(int(data[id_elt]))]
+
+    positions_points_id_elt = [offset + int(nb_blocks) + i for i in range(int(nb_free_space))]
+
+    positions_points.append([positions_points_id_elt, 0, nb_free_space])
+    offset += int(nb_blocks) + nb_free_space
+if len(data) % 2 != 0:
+    positions[str(len(data) // 2)] = [offset + i for i in range(int(data[-1]))]
+
+keys = sorted(list(positions.keys()), key=lambda x: int(x))[::-1]
+indexes_to_ignore = set()
+
+for id_number, number in enumerate(keys):
+    pos_number = positions[number]
+    nb_files = len(pos_number)
+    if id_number not in indexes_to_ignore:
+        for id_space, space in enumerate(positions_points):
+            if id_space < int(number):
+                indexes = space[0]
+                lengthtot = space[2]
+                index_start = space[1]
+
+                space_left = lengthtot - index_start
+
+                if space_left >= nb_files:
+                    positions[number] = indexes[index_start:index_start + nb_files]
+
+                    space[1] = space[1] + nb_files
+                    indexes_to_ignore.add(id_number)
+                    break
+
+part2 = sum(int(elt) * val for elt in positions.keys() for val in positions[elt])
+print(part2)
