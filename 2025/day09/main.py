5f4ba611cb8afbaf74a47189b6476c18b1b279ee
rirst <alexandre_boyer63@yahoo.fr>
2025-12-10 22:20:57 +0100

    day 09 aoc part 2

--- 2025/day09/main.py
+++ 2025/day09/main.py
@@ -1,31 +1,25 @@
-
+from itertools import pairwise
 points = [list(map(int, elt.split(","))) for elt in open("day09/input").readlines()]
-points = [i[0] +1j*i[1] for i in points]
-areas = [(abs(x.real-y.real)+1) * (abs(x.imag-y.imag)+1) for x in points for y in points]
-center_real = sum([x.real for x in points]) / len(points)
-center_imag = sum([x.imag for x in points]) / len(points)
-print(max(areas))
-def manhattan(number):
-    return abs(number.real-center_real) + abs(number.imag - center_imag)
+points = [i[1] +1j*i[0] for i in points]
+def area(pos1, pos2):
+    return (abs(pos1.real-pos2.real)+1) * (abs(pos1.imag-pos1.imag)+1)
+areas = set(((x,y), (abs(x.real-y.real)+1) * (abs(x.imag-y.imag)+1)) for x in points for y in points)
+print(max(areas, key = lambda x : x[-1]))
 # part2
-candidates = []
-for p1 in points:
-    for p2 in set(points) - {p1}:
-        rect = {min(p1.real, p2.real) + 1j*min(p1.imag, p2.imag),
-                max(p1.real, p2.real) + 1j*min(p1.imag, p2.imag),
-                max(p1.real, p2.real) + 1j*max(p1.imag, p2.imag),
-                min(p1.real, p2.real) + 1j*max(p1.imag, p2.imag)} - {p1, p2}
-        to_add = 0
-        for c in rect:
-            p = [p for p in set(points) - {p1,p2} if p.real == c.real or c.imag == p.imag]
-            distances = [manhattan(c) - manhattan(v) for v in p]
-            if all(dist <= 0 for dist in distances):
-                to_add += 1
-        if to_add == 2:
-            import matplotlib.pyplot as plt
-            plt.plot([x.real for x in points], [x.imag for x in points])
-            plt.scatter(p1.real, p1.imag, marker="x")
-            plt.scatter(p2.real, p2.imag, marker="x")
-            plt.show()
-            candidates.append((abs(p1.real-p2.real)+1) * (abs(p1.imag-p2.imag)+1))
-print(int(max(candidates)))
+edges = [(points[i], points[i-1]) for i in range(len(points))]
+for pos, ar in sorted(areas, key=lambda x : x[-1], reverse=True):
+    p1, p2 = pos
+    x1, y1 = p1.real, p1.imag
+    x2, y2 = p2.real, p2.imag
+    x1, x2 = sorted((x1, x2))
+    y1, y2 = sorted((y1, y2))
+    count = 0
+    for e1, e2 in edges:
+        x3, x4 = e1.real, e2.real
+        y3, y4 = sorted((e1.imag, e2.imag))
+        if x4 > x1 and x3< x2 and y4 > y1 and y3 < y2:
+            count += 1
+            break
+    if count == 0:
+        print(ar)
+        break
